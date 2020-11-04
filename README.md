af45ab0db8d20d81e6176f1d2fd35ef39393d27b
unknown <jlmamaniq@gmail.com>
2020-11-03 20:37:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# postme
+# postme
---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+/* Declaraion de variables globales */
+let MAIN;
+let MODAL_POST;
+let BTN_SHOW_POST;
+let BTN_CANCEL_POST;
+
+
+// Funciones
+// Cuando se cargue todo nuestro DOM
+
+
+const showPostModal = () => {
+    console.log('Elementos Modificados');
+    MAIN.style.display = 'none';
+
+    MODAL_POST.style.display = 'block';
+
+    setTimeout(() => {
+        MODAL_POST.style.transform = 'translateY(0)';
+    }, 1);
+};
+const closePostModal = () => {
+    MAIN.style.display = 'block';
+    MODAL_POST.style.transform = 'translateY(100vh)';
+};
+
+window.addEventListener('load', () => {
+    MAIN = document.querySelector('#main');
+    MODAL_POST = document.querySelector('#modal-post-section');
+    BTN_SHOW_POST = document.querySelector('#btn-upload-post');
+    BTN_SHOW_POST.addEventListener('click', showPostModal);
+    BTN_CANCEL_POST = document.querySelector('#btn-post-cancel');
+    BTN_CANCEL_POST.addEventListener('click', closePostModal)
+    console.log('Elementos Cargados');
+});
