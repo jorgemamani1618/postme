09d37013b4a1f2875cccf60ab7f707975b4b6a40
unknown <jlmamaniq@gmail.com>
2020-11-03 22:17:33 -0500

    Modificaciones PWA

--- src/js/app.js
+++ src/js/app.js
@@ -10,7 +10,7 @@ let BTN_CANCEL_POST;
 
 
 const showPostModal = () => {
-    console.log('Elementos Modificados');
+
     MAIN.style.display = 'none';
 
     MODAL_POST.style.display = 'block';
@@ -24,7 +24,7 @@ const closePostModal = () => {
     MODAL_POST.style.transform = 'translateY(100vh)';
 };
 
-window.addEventListener('load', () => {
+window.addEventListener('load', async() => {
     MAIN = document.querySelector('#main');
     MODAL_POST = document.querySelector('#modal-post-section');
     BTN_SHOW_POST = document.querySelector('#btn-upload-post');
@@ -32,4 +32,17 @@ window.addEventListener('load', () => {
     BTN_CANCEL_POST = document.querySelector('#btn-post-cancel');
     BTN_CANCEL_POST.addEventListener('click', closePostModal)
     console.log('Elementos Cargados');
+
+
+    if ('serviceWorker' in navigator) {
+        if (navigator.serviceWorker) {
+            const response = await navigator.serviceWorker.register('sw.js');
+            if (response) {
+                console.info('service worker registrado');
+            }
+
+        }
+    }
+
+
 });
